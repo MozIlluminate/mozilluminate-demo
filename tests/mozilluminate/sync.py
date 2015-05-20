ca69ceba4366e99c04decd0c919cb22f8bf603df
Shing Lyu <shing.lyu@gmail.com>
2015-05-20 14:53:41 +0800

    Sort out relative path

--- tests/mozilluminate/sync.py
+++ tests/mozilluminate/sync.py
@@ -4,14 +4,17 @@ import os
 
 #TODO: eliminate this tmp file
 tmpfile="tmp.txt"
+script_dir = os.path.dirname(os.path.realpath(__file__))
 
 #FIXME: hardcoded feature file name
-cmd = "%s %s -r %s > %s" % ('./moztrap_integration/cucumber-js/bin/cucumber.js', 'fxos.rocketbar.feature',
-                            './moztrap_integration/step_definitions/moztrap.steps.js', tmpfile)
-os.system(cmd)
+testcase_files = [script_dir + "/../../apps/search/test/manual/rocketbar.md"]
+
+for testcase_file in testcase_files:
+    cmd = "%s %s -r %s > %s" % (script_dir + '/moztrap_integration/cucumber-js/bin/cucumber.js', testcase_file,
+                                script_dir + '/moztrap_integration/step_definitions/moztrap.steps.js', tmpfile)
+    os.system(cmd)
 #TODO: detect for changed/added/delete case only and ignore the rest
 
-#FIXME: hardcoded username/apikey
-mtapi.mtorigin = "https://moztrap-dev.allizom.org"
-mtapi.convert_mark_file_into_moztrap(tmpfile, {'username': os.getenv("mz_user_name"), 'api_key': os.getenv("mz_api_key")})
-
+    mtapi.mtorigin = "https://moztrap-dev.allizom.org"
+#The mz_user_name and mz_api_key is set in Travis CI
+    mtapi.convert_mark_file_into_moztrap(tmpfile, {'username': os.getenv("mz_user_name"), 'api_key': os.getenv("mz_api_key")})
