310c27ef9ead916600470a3741c3004947bdbfba
Shing Lyu <shing.lyu@gmail.com>
2015-07-15 14:57:22 +0800

    Enable moving case between suite detection; flattens case obj

--- tests/mozilluminate/sync.py
+++ tests/mozilluminate/sync.py
@@ -8,51 +8,44 @@ import json
 #TODO: eliminate this tmp file
 #tmpfile="tmp.txt"
 
+def flatten(suites):
+    flattened = []
+    for suite in suites:
+        for testcase in suite['testcases']:
+            testcase['suites'] = [suite['name']]
+            flattened.append(testcase)
+    return flattened
+
 def summarize_diff(before, after):
     added = []
     modified = []
     removed = []
 
-    suite_pairs = map(None,
-        sorted(before, key=lambda x: x['name']),
-        sorted(after, key=lambda x: x['name'])
-        )
-    for suite_pair in suite_pairs:
-        if suite_pair[0] is None:
-            raise NotImplementedError
-            #added suite
-        elif suite_pair[1] is None:
-            raise NotImplementedError
-            #removed suite
-        else:
-            intersect_titles = list( # cases that are in both before and after, may be a modified or not modified case
-                set(map(lambda x: x['id'], suite_pair[0]['testcases'])) &
-                set(map(lambda x: x['id'], suite_pair[1]['testcases']))
-            )
-            adds = [ x for x in suite_pair[1]['testcases']
-                        if x not in suite_pair[0]['testcases']
-                            and x['id'] not in intersect_titles]
-            removes = [ x for x in suite_pair[0]['testcases']
-                          if x not in suite_pair[1]['testcases']
-                                and x['id'] not in intersect_titles]
-            modifies = [ x for x in suite_pair[1]['testcases']
-                            if x not in suite_pair[0]['testcases']
-                                and x['id'] in intersect_titles]
-            if (len(adds) > 0):
-                added.append({
-                    'name': suite_pair[0]['name'],
-                    'testcases': adds
-                })
-            if (len(removes) > 0):
-                removed.append({
-                    'name': suite_pair[0]['name'],
-                    'testcases': removes
-                })
-            if (len(modifies) > 0):
-                modified.append({
-                    'name': suite_pair[0]['name'],
-                    'testcases': modifies
-                })
+#    suite_pairs = map(None,
+#        sorted(before, key=lambda x: x['name']),
+#        sorted(after, key=lambda x: x['name'])
+#        )
+#    for suite_pair in suite_pairs:
+#    if suite_pair[0] is None:
+#        raise NotImplementedError
+#        #added suite
+#    elif suite_pair[1] is None:
+#        raise NotImplementedError
+#        #removed suite
+#    else:
+    intersect_titles = list( # cases that are in both before and after, may be a modified or not modified case
+        set(map(lambda x: x['id'], before)) &
+        set(map(lambda x: x['id'], after))
+    )
+    added += [ x for x in after
+                if x not in before
+                    and x['id'] not in intersect_titles]
+    removed += [ x for x in before
+                    if x not in after
+                        and x['id'] not in intersect_titles]
+    modified += [ x for x in after
+                    if x not in before
+                        and x['id'] in intersect_titles]
 
     return (added, modified, removed)
 
@@ -71,8 +64,8 @@ def main():
 #testcase_files = [script_dir + "/../../apps/search/test/manual/rocketbar.md"]
 #testcase_files = [script_dir + "/../../apps/sms/test/manual/README.md"]
 
-#repo = Repo('../../')
-    repo = Repo('../../../../') #for debuggin
+    repo = Repo('../../')
+   # repo = Repo('../../../../') #for debuggin
 
     updated_diffs = filter(lambda x: "apps" in x.b_path and "test/manual" in x.b_path, repo.commit('HEAD~1').diff('HEAD'))
 
@@ -80,65 +73,44 @@ def main():
 
 #for testcase_file in testcase_files:
     for diff in updated_diffs:
-        if diff.new_file: #&& edit only
-            raise NotImplementedError
+        #how to deteced diff type:
+        #if diff.new_file: #&& edit only
+        #elif diff.deleted_file:
+        #elif diff.renamed:
+        #else:
 
+        testcase_before = tempfile.NamedTemporaryFile()
+        testcase_after = tempfile.NamedTemporaryFile()
 
-#TODO: detect for changed/added/delete case only and ignore the rest
-
-        elif diff.deleted_file:
-            raise NotImplementedError
-            #TODO
-        elif diff.renamed:
-            raise NotImplementedError
-            #TODO
-        else:
-
-            #cmd = "%s %s -r %s > %s" % (script_dir + '/moztrap_integration/cucumber-js/bin/cucumber.js', testcase_file,
-            #                            script_dir + '/moztrap_integration/step_definitions/moztrap.steps.js', tmpfile)
-            # subprocesscheck_output(args, *, stdin=None, stderr=None, shell=False, universal_newlines=False).
-            #parsed_json_f = tempfile.NamedTemporaryFile()
-            testcase_before = tempfile.NamedTemporaryFile()
-            testcase_after = tempfile.NamedTemporaryFile()
-
+        try:
             testcase_before.write(diff.a_blob.data_stream.read())
-            testcase_before.flush()
-
+        except AttributeError:
+            testcase_before.write(json.dumps([]))
+        try:
             testcase_after.write(diff.b_blob.data_stream.read())
-            testcase_after.flush()
+        except AttributeError:
+            testcase_before.write(json.dumps([]))
 
-            #cmd = "%s %s > %s" % (script_dir + '/moztrap_integration/markdown-testfile-to-json/cli.js', testcase_file,
-            #                    parsed_json_f.name)
-            #os.system(cmd)
-            #print subprocess.check_output(['cat', testcase_before.name])
-            before_json = json.loads(subprocess.check_output([script_dir + '/moztrap_integration/markdown-testfile-to-json/cli.js', testcase_before.name]))
-            after_json = json.loads(subprocess.check_output([script_dir + '/moztrap_integration/markdown-testfile-to-json/cli.js', testcase_after.name]))
+        testcase_before.flush()
+        testcase_after.flush()
 
-            pairs = map(None,
-                sorted(before_json, key=lambda x: x['name']),
-                sorted(after_json, key=lambda x: x['name'])
-                )
-            print "preparing paris"
-            print pairs
-            for pair in pairs:
-                if pair[0] is None:
-                    raise NotImplementedError
-                    #added suite
-                if pair[1] is None:
-                    raise NotImplementedError
-                    #removed suite
+        #cmd = "%s %s > %s" % (script_dir + '/moztrap_integration/markdown-testfile-to-json/cli.js', testcase_file,
+        #                    parsed_json_f.name)
+        #os.system(cmd)
+        #print subprocess.check_output(['cat', testcase_before.name])
+        before_json += json.loads(subprocess.check_output([script_dir + '/moztrap_integration/markdown-testfile-to-json/cli.js', testcase_before.name]))
+        after_json += json.loads(subprocess.check_output([script_dir + '/moztrap_integration/markdown-testfile-to-json/cli.js', testcase_after.name]))
 
-                #added = list(set(new) - set(old))
-                #removed= list(set(old) - set(new))
-                #for
-            #print before_json
+        diff = summarize_diff(before_json, after_json)
 
-    parsed_json_f = tempfile.NamedTemporaryFile()
+
+    #parsed_json_f = tempfile.NamedTemporaryFile()
 
     mtapi.mtorigin = "https://moztrap-dev.allizom.org"
 #The mz_user_name and mz_api_key is set in Travis CI
 #mtapi.convert_mark_file_into_moztrap(tmpfile, {'username': os.getenv("mz_user_name"), 'api_key': os.getenv("mz_api_key")})
-    mtapi.load_json_into_moztrap(parsed_json_f.name, {'username': os.getenv("mz_user_name"), 'api_key': os.getenv("mz_api_key")})
+    #mtapi.load_json_into_moztrap(parsed_json_f.name, {'username': os.getenv("mz_user_name"), 'api_key': os.getenv("mz_api_key")})
+    mtapi.sync_diff_to_moztrap(diff, {'username': os.getenv("mz_user_name"), 'api_key': os.getenv("mz_api_key")})
 
 if __name__ == '__main__':
     main()
--- tests/mozilluminate/test/unit/test_sync.py
+++ tests/mozilluminate/test/unit/test_sync.py
@@ -25,20 +25,17 @@ class TestSync(unittest.TestCase):
         with open('after_add_case.json', 'r') as af:
             after_json = json.load(af)
 
-        adds, modifies, removes = sync.summarize_diff(before_json, after_json)
+        adds, modifies, removes = sync.summarize_diff(sync.flatten(before_json),
+                                                      sync.flatten(after_json))
 
         expected_adds= [{
-            "name": "Launch suite",
-            "testcases": [
-                {
-                    "bug": 2,
-                    "id": "fxos.func.sanity.launch_foobar",
-                    "instructions": "Launch FOOBAR",
-                    "state": "draft",
-                    "userStory": 1
-                }
-            ]
-        }]
+                            "bug": 2,
+                            "id": "fxos.func.sanity.launch_foobar",
+                            "instructions": "Launch FOOBAR",
+                            "state": "draft",
+                            "userStory": 1,
+                            "suites": ["Launch suite"],
+                        }]
         self._assertResult(adds, modifies, removes,
                            exp_adds= expected_adds)
 
@@ -48,20 +45,17 @@ class TestSync(unittest.TestCase):
         with open('after_remove_case.json', 'r') as af:
             after_json = json.load(af)
 
-        adds, modifies, removes = sync.summarize_diff(before_json, after_json)
+        adds, modifies, removes = sync.summarize_diff(sync.flatten(before_json),
+                                                      sync.flatten(after_json))
 
         expected_removes = [{
-                            "name": "Launch suite",
-                            "testcases": [
-                                {
-                                    "bug": 4,
-                                    "id": "fxos.func.sanity.launch_settings",
-                                    "instructions": "Launch settings, oh yeah",
-                                    "state": "draft",
-                                    "userStory": 3
-                                }
-                            ]
-                        }]
+                                "bug": 4,
+                                "id": "fxos.func.sanity.launch_settings",
+                                "instructions": "Launch settings, oh yeah",
+                                "state": "draft",
+                                "userStory": 3,
+                                "suites": ["Launch suite"],
+                            }]
         self._assertResult(adds, modifies, removes,
                            exp_removes = expected_removes)
 
@@ -71,22 +65,95 @@ class TestSync(unittest.TestCase):
         with open('after_modify_case.json', 'r') as af:
             after_json = json.load(af)
 
-        adds, modifies, removes = sync.summarize_diff(before_json, after_json)
+        adds, modifies, removes = sync.summarize_diff(sync.flatten(before_json),
+                                                      sync.flatten(after_json))
 
         expected_modifies= [{
-                            "name": "Launch suite",
-                            "testcases": [
-                                {
-                                    "bug": 2,
-                                    "id": "fxos.func.sanity.launch_contacts",
-                                    "instructions": "Launch contacts! OH I MODIFIED THIS!!!",
-                                    "state": "draft",
-                                    "userStory": 1
-                                }
-                            ]
-                        }]
+                                "bug": 2,
+                                "id": "fxos.func.sanity.launch_contacts",
+                                "instructions": "Launch contacts! OH I MODIFIED THIS!!!",
+                                "state": "draft",
+                                "userStory": 1,
+                                "suites": ["Launch suite"],
+                            }]
         self._assertResult(adds, modifies, removes,
                            exp_modifies= expected_modifies)
+
+    def test_summarize_diff_add_suite_and_move_case_between_suite(self):
+        with open('before.json', 'r') as bf:
+            before_json = json.load(bf)
+        with open('after_move_between_suite.json', 'r') as af:
+            after_json = json.load(af)
+
+        adds, modifies, removes = sync.summarize_diff(sync.flatten(before_json),
+                                                      sync.flatten(after_json))
+
+        expected_modifies = [{
+                                "bug": 4,
+                                "id": "fxos.func.sanity.launch_settings",
+                                "instructions": "Launch settings, oh yeah",
+                                "state": "draft",
+                                "userStory": 3,
+                                "suites": ["Close suite"]
+                            }]
+        self._assertResult(adds, modifies, removes,
+                           exp_modifies= expected_modifies)
+
+    def test_summarize_diff_delete_suite(self):
+        with open('before.json', 'r') as bf:
+            before_json = json.load(bf)
+
+        after_json = []
+
+        adds, modifies, removes = sync.summarize_diff(sync.flatten(before_json),
+                                                      sync.flatten(after_json))
+
+        expected_removes = [
+            {
+                "bug": 2,
+                "id": "fxos.func.sanity.launch_contacts",
+                "instructions": "Launch contacts! this is an updated version",
+                "state": "draft",
+                "userStory": 1,
+                "suites": ["Launch suite"]
+            },
+            {
+                "bug": 4,
+                "id": "fxos.func.sanity.launch_settings",
+                "instructions": "Launch settings, oh yeah",
+                "state": "draft",
+                "userStory": 3,
+                "suites": ["Launch suite"]
+            }
+        ]
+        self._assertResult(adds, modifies, removes,
+                           exp_removes= expected_removes)
+
+    def test_flatten(self):
+        with open('before.json', 'r') as bf:
+            before_json = json.load(bf)
+
+
+        expected_out = [
+            {
+                "bug": 2,
+                "id": "fxos.func.sanity.launch_contacts",
+                "instructions": "Launch contacts! this is an updated version",
+                "state": "draft",
+                "userStory": 1,
+                "suites": ["Launch suite"]
+            },
+            {
+                "bug": 4,
+                "id": "fxos.func.sanity.launch_settings",
+                "instructions": "Launch settings, oh yeah",
+                "state": "draft",
+                "userStory": 3,
+                "suites": ["Launch suite"]
+            }
+        ]
+
+        self.assertEqual(sync.flatten(before_json), expected_out)
 
 if __name__ == '__main__':
     unittest.main()
